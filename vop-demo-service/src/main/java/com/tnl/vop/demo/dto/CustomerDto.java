82cc47c2915e17f64abfb4558d986604934d3dcd
Mohanraj Kathiravan <mohanraj.kathiravan@va.gov>
2025-11-12 11:52:59 -0500

    request example

--- vop-demo-service/src/main/java/com/tnl/vop/demo/dto/CustomerDto.java
+++ vop-demo-service/src/main/java/com/tnl/vop/demo/dto/CustomerDto.java
@@ -1,10 +1,21 @@
 package com.tnl.vop.demo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record CustomerDto(
-        @NotBlank String firstName,
-        @NotBlank String lastName,
-        @Email String email
-) {}
+    @NotBlank
+    @Schema(example = "Ada")
+    String firstName,
+
+    @NotBlank
+    @Schema(example = "Lovelace")
+    String lastName,
+
+    @Email
+    @Schema(example = "ada.lovelace@example.com")
+    String email
+) {
+
+}
